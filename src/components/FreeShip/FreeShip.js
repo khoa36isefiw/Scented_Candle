084b656fc76b201e2d14b9c8cd025383b250b833
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-01-01 15:34:17 +0700

    make free ship and install animation for css

--- src/components/FreeShip/FreeShip.js
+++ src/components/FreeShip/FreeShip.js
@@ -4,7 +4,7 @@ function FreeShip() {
     return (
         <Box className="bg-[#a7bda0] h-[40px] w-full text-center justify-center flex items-center">
             <div>
-                <span className="text-[18px] text-white px-4">
+                <span className="text-[16px] text-white px-4">
                     <span className="font-bold">FREE SHIPPING</span> on orders over $150
                 </span>
                 <span>🚚</span>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import FreeShip from '../components/FreeShip/FreeShip';
+import Header from '../components/Header/Header';
 
 function HomePage() {
     return (
         <div>
             <FreeShip />
+            <Header />
         </div>
     );
 }

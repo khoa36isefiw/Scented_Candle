e1c30be3c725bd298ac75ae7d8166a118d30defd
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-01-11 17:12:50 +0700

    re-make button back to top

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { privateRoutes, publicRoutes } from './routes/routes';
+// import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+
+// function App() {
+//     return (
+//         <Router>
+//             <div>
+//                 <Routes>
+//                     {publicRoutes.map((route, index) => {
+//                         const Page = route.component;
+//                         let Layout = route.layout;
+//                         if (!Layout) {
+//                             Layout = React.Fragment;
+//                         }
+//                         const type = route.type || '';
+//                         return (
+//                             <Route
+//                                 key={index}
+//                                 path={route.path}
+//                                 element={
+//                                     <Layout>
+//                                         <Page type={type} />
+//                                     </Layout>
+//                                 }
+//                             ></Route>
+//                         );
+//                     })}
+
+//                     {privateRoutes.map((route, index) => {
+//                         const Page = route.component;
+//                         let Layout = route.layout;
+//                         if (!Layout) {
+//                             Layout = React.Fragment;
+//                         }
+//                         return (
+//                             <Route
+//                                 key={index}
+//                                 path={route.path}
+//                                 element={
+//                                     <Layout>
+//                                         <Page />
+//                                     </Layout>
+//                                 }
+//                             ></Route>
+//                         );
+//                     })}
+//                 </Routes>
+//                 <ScrollToTop />
+//             </div>
+//         </Router>
+//     );
+// }
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from './routes/routes';
-import ScrollToTopButton from './components/ScrollToTop/ScrollToTop';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
     return (
@@ -47,8 +104,8 @@ function App() {
                         );
                     })}
                 </Routes>
+                <ScrollToTop />
             </div>
-            <ScrollToTopButton />
         </Router>
     );
 }
